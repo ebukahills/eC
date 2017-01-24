37e5dc1702676a1e2c2492c79496c097f9ae51af
Ebuka Hills <flyg101@gmail.com>
2017-01-24 01:06:41 +0100

    ticker update

--- src/components/MainPriceTicker.js
+++ src/components/MainPriceTicker.js
@@ -8,12 +8,27 @@ import {
   Pager,
   FormGroup,
   FormControl,
+  FieldGroup,
   Checkbox,
   Accordion,
   Panel
 } from 'react-bootstrap';
 
 import FontAwesome from 'react-fontawesome';
+
+// Create Terms of Service TOS Component
+const TOS = React.createClass({
+  render: () => {
+    return (
+      <Accordion>
+        <Panel header={'click to read TERMS OF SERVICE'} eventKey='1' >
+          <p>Terms of Service and Trade</p>
+        </Panel>
+      </Accordion>
+    )
+  }
+})
+
 
 class MainPriceChecker extends Component {
 
@@ -23,12 +38,20 @@ class MainPriceChecker extends Component {
 
     //Bind Component functions to constructor
     this.loadMainPage = this.loadMainPage.bind(this)
+
     this.loadBuyPage1 = this.loadBuyPage1.bind(this)
     this.loadBuyPage2 = this.loadBuyPage2.bind(this)
     this.loadBuyPage3 = this.loadBuyPage3.bind(this)
+
     this.loadSellPage1 = this.loadSellPage1.bind(this)
     this.loadSellPage2 = this.loadSellPage2.bind(this)
     this.loadSellPage3 = this.loadSellPage3.bind(this)
+
+    this.loadBuyComplete = this.loadBuyComplete.bind(this)
+    this.loadBuyCancel = this.loadBuyCancel.bind(this)
+    this.loadSellComplete = this.loadSellComplete.bind(this)
+    this.loadSellCancel = this.loadSellCancel.bind(this)
+
   }
 
   loadMainPage() {
@@ -54,6 +77,27 @@ class MainPriceChecker extends Component {
     this.setState({ tPage: 'sellPage3' })
     // This function also adds the transaction values to database
   }
+  loadBuyComplete() {
+    // TODO - Set transaction on db to PROCESSING
+
+    this.setState({ tPage: 'buyComplete' })
+
+  }
+  loadBuyCancel() {
+    // TODO - Set transaction on db to CANCELLED
+
+    this.setState({ tPage: 'buyCancel' })
+  }
+  loadSellComplete() {
+    //TODO - Set transaction in db to PROCESSING
+
+    this.setState({ tPage: 'sellComplete' })
+  }
+  loadSellCancel() {
+    // TODO - Set transaction in db to CANCELLED
+
+    this.setState({ tPage: 'sellCancel' })
+  }
 
   render() {
 
@@ -62,21 +106,13 @@ class MainPriceChecker extends Component {
     var mainPage = (
       <div>
         <p>Select your preferred Transaction below:</p>
-        <Col md={6} sm={6} xm={6} xs={6} style={{ textAlign: 'right', borderRight: 30 }} >
-          <FontAwesome
-            name='bitcoin'
-            size='5x'
-            style={{ textShadow: '0 5px 0 rgba(0, 0, 0, 0.1)' }}
-            /><br />
+        <Col md={6} sm={6} xs={6} style={{ textAlign: 'right', borderRight: 30 }} >
+          <br />
           <Button bsSize='large' bsStyle='success' onClick={this.loadBuyPage1} >BUY</Button>
         </Col>
 
-        <Col md={6} sm={6} xm={6} xs={6} style={{ textAlign: 'left', borderLeft: 30 }} >
-          <FontAwesome
-            name='bitcoin'
-            size='5x'
-            style={{ textShadow: '0 5px 0 rgba(0, 0, 0, 0.1)' }}
-            /><br />
+        <Col md={6} sm={6} xs={6} style={{ textAlign: 'left', borderLeft: 30 }} >
+          <br />
           <Button bsSize='large' bsStyle='info' onClick={this.loadSellPage1} >SELL</Button>
         </Col>
       </div>
@@ -129,16 +165,16 @@ class MainPriceChecker extends Component {
           <p><b>Account Name:</b>  Gbaski Sales Ltd.</p><hr />
           <p><b>Account Number:</b>  0123456789</p><hr />
           <p><b>Amount:</b>  45000 NGN</p><hr />
-          <p><b>Transaction Reference:</b>  4582389</p><hr />
+          <p><b>Transaction Reference:</b>  458T2P3W89</p><hr />
         </Panel>
         <Accordion>
           <Panel header={(<h5>Complete/Cancel Transaction</h5>)}>
             <p>After paying the specified Amount into the Bank Account, you can click the 'Payment Complete' button below.</p>
-            <p>If however, you do not plan to complete this transaction, click the 'Cancel' button below, at no charge</p><hr/>
+            <p>If however, you do not plan to complete this transaction, click the 'Cancel' button below, at no charge</p><hr />
             <p>To help us process your Transaction faster and more efficiently, kindly upload a proof of payment (picture of bank teller, receipt, bank statement, mobile app screenshot etc) on the next page</p>
             <p>Or send it as an email attachment to <a href="mailto:paid@echange.ng?Subject=eChange%20Payment%20Proof" target='_blank'>paid@eChange.NG</a></p>
-            <hr/>
-            <Button bsStyle='success' bsSize='large' >Payment Complete</Button>{'  '} <Button bsStyle='danger' bsSize='large' >Cancel</Button>
+            <hr />
+            <Button bsStyle='success' bsSize='large' onClick={this.loadBuyComplete} >Payment Complete</Button>{'  '} <Button bsStyle='danger' bsSize='large' onClick={this.loadBuyCancel} >Cancel</Button>
           </Panel>
         </Accordion>
       </div>
@@ -151,9 +187,9 @@ class MainPriceChecker extends Component {
         <h3>SellPriceTicker</h3>
         <br />
         <Pager>
-          <Pager.Item onSelect={this.loadMainPage} >BACK</Pager.Item>
+          <Pager.Item onSelect={this.loadMainPage} >&larr; BACK</Pager.Item>
           {'   '}
-          <Pager.Item onSelect={this.loadSellPage2} >NEXT</Pager.Item>
+          <Pager.Item onSelect={this.loadSellPage2} >NEXT &rarr;</Pager.Item>
         </Pager>
       </div>
     );
@@ -162,50 +198,66 @@ class MainPriceChecker extends Component {
 
     var sellPage3;
 
+    var buyComplete = (
+      <div>
+        <h5>Transaction 98UGDG009</h5>
+        <p>Receiving Address: 16dAwJttDm5oiSAZUYMqgWWGHso1jjSg9C</p><hr />
+        <p>You have marked this Transaction as Paid</p>
+        <p>Kinldy upload a proof of payment below (teller, receipt, app screenshot, etc)</p>
+        <p>This ensures your transaction will be processed without delay <FontAwesome name='clock-o' size='2x' /></p><hr />
+        <label className="btn btn-primary">
+          <input id="my-file-selector" type="file" style={{display:'none'}} />
+          <FontAwesome name='upload' /> UPLOAD PROOF
+        </label>
+          <Pager>
+            <Pager.Item onSelect={this.loadMainPage} >&larr; Back to Dashboard</Pager.Item>
+          </Pager>
+      </div>
+        );
+    var buyCancel;
+    var sellComplete;
+    var sellCancel;
+
 
     // Condtionally render Price Ticker Page
     // by setting the value of tickerPage to page variable based on state
     if (this.state.tPage === 'mainPage') {
-      tickerPage = mainPage;
-    } else if (this.state.tPage === 'buyPage1') {
-      tickerPage = buyPage1;
-    } else if (this.state.tPage === 'buyPage2') {
-      tickerPage = buyPage2;
-    } else if (this.state.tPage === 'buyPage3') {
-      tickerPage = buyPage3;
-    } else if (this.state.tPage === 'sellPage1') {
-      tickerPage = sellPage1;
-    } else if (this.state.tPage === 'sellPage2') {
-      tickerPage = sellPage2
-    } else if (this.state.tPage === 'sellPage3') {
-      tickerPage = sellPage3;
-    } else {
-      tickerPage = mainPage;
-    }
+          tickerPage = mainPage;
+        } else if (this.state.tPage === 'buyPage1') {
+          tickerPage = buyPage1;
+        } else if (this.state.tPage === 'buyPage2') {
+          tickerPage = buyPage2;
+        } else if (this.state.tPage === 'buyPage3') {
+          tickerPage = buyPage3;
+        } else if (this.state.tPage === 'sellPage1') {
+          tickerPage = sellPage1;
+        } else if (this.state.tPage === 'sellPage2') {
+          tickerPage = sellPage2
+        }else if (this.state.tPage === 'sellPage3') {
+          tickerPage = sellPage3;
+        } else if (this.state.tPage === 'buyComplete') {
+          tickerPage = buyComplete;
+        } else if (this.state.tPage === 'buyCancel') {
+          tickerPage = buyCancel;
+        } else if (this.state.tPage === 'sellComplete') {
+          tickerPage = sellComplete;
+        } else if (this.state.tPage === 'sellCancel') {
+          tickerPage = sellCancel;
+        }else {
+          tickerPage = mainPage;
+        }
 
     return (
       <div>
-        <Well>
-          <Row className='clearfix'>
-            {tickerPage}
-          </Row>
-        </Well>
-      </div>
-    )
+          <Well>
+            <Row className='clearfix'>
+              {tickerPage}
+            </Row>
+          </Well>
+        </div>
+        )
   }
 }
 
-// Create Terms of Service TOS Component
-const TOS = React.createClass({
-  render: () => {
-    return (
-      <Accordion>
-        <Panel header={'click to read TERMS OF SERVICE'} eventKey='1' >
-          <p>Terms of Service and Trade</p>
-        </Panel>
-      </Accordion>
-    )
-  }
-})
 
 export default MainPriceChecker
